4bd8bf4211b5ad5894c943bb153c644db9f2a04e
Tiago <t.piontkewicz@gmail.com>
2021-01-22 23:49:14 -0300

    Adição de controle de volume

--- app/src/main/java/com/example/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.mediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar skVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,38 @@ public class MainActivity extends AppCompatActivity {
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.takebackthepower);
 
+        configSeekVolume();
+
+    }
+
+    private void configSeekVolume(){
+        skVolume = findViewById(R.id.skVolume);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //recuperar o valor de volume máximo e atual
+        int volumeMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        skVolume.setMax(volumeMax);
+        skVolume.setProgress(volumeAtual);
+
+        skVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void playMusic(View view){
@@ -35,6 +72,33 @@ public class MainActivity extends AppCompatActivity {
         if ( mediaPlayer.isPlaying() ){
             mediaPlayer.stop();
             mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.takebackthepower);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if ( mediaPlayer.isPlaying() ){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if ( mediaPlayer != null ){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if( mediaPlayer != null && mediaPlayer.isPlaying() ){
+            mediaPlayer.stop();
+            //libera espaço de memória
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
 }
